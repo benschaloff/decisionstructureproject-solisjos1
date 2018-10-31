0fece2341a5f5c7e31e013adffeab2e17e7c0fd1
schalben <schalben@011-4303-S16XH.students.cisd.conroeisd.loc>
2018-10-31 07:34:54 -0500

    I said hello to you

---
+++ OtherProject/src/otherproject/OtherProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package otherproject;
+
+/**
+ *
+ * @author schalben
+ */
+public class OtherProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //This is BigBen, hello
+    }
+    
+}
